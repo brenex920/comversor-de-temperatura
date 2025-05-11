5b3642e29c5a57540de4ed5406cfe70181f54d92
Breno Machado <machado.breno127@gmail.com>
2025-05-11 20:27:50 -0300

    projeto pronto

--- src/br/dev/breno/temperatura/gui/ConversaoFrame.java
+++ src/br/dev/breno/temperatura/gui/ConversaoFrame.java
@@ -1,7 +1,6 @@
 package br.dev.breno.temperatura.gui;
 
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.text.ParseException;
 import javax.swing.JButton;
 import javax.swing.JFormattedTextField;
@@ -44,33 +43,38 @@ public class ConversaoFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //parte de dentro da tela
-        setLayout(new FlowLayout());
+        setLayout(null);
         
         // add(new JLabel("Temperatura em C"));
         // add(new JTextField(10));
 
         this.txt = new JLabel("valor de celcius");
+        this.txt.setBounds(50, 40, 150, 50);
         add(this.txt);
 
         var maskFormat = new MaskFormatter("###");
         this.vlrC = new JFormattedTextField(maskFormat);
         this.vlrC.setPreferredSize(new Dimension(150, 20));
         this.vlrC.setFocusLostBehavior(JFormattedTextField.PERSIST);
+        this.vlrC.setBounds(50, 75, 150, 20);
         add(this.vlrC);
         
         // metodo facil
         this.btnF = new JButton("F");
         this.btnF.addActionListener(event -> this.conversaoT("F"));
+        this.btnF.setBounds(130, 100, 75, 35);
         add(this.btnF);
     
         // metodo dificil
         this.btnK = new JButton("K");
         this.btnK.addActionListener(evet -> this.conversaoT("K"));
-        
+        this.btnK.setBounds(50, 100, 75, 35);
         add(this.btnK);
         
-        this.valor = new JLabel("Valor em C: " + this.vlrC.getText());
-        this.valor.setBounds(100, 111, 75, 35);
+        this.valor = new JLabel(" " + this.vlrC.getText());
+        this.valor.setBounds(100, 150, 130, 35);
         add(this.valor);
+
+        setVisible(true);
     }
 }
